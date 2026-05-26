b89c18f4e25927ed504e6fb66d91a0439d903d6f
velalabasso <velalab.asso@gmail.com>
2026-05-26 13:06:01 +0200

    Update velacarto.js

--- velacarto.js
+++ velacarto.js
@@ -854,7 +854,7 @@ window.VelaCarto = {
       });
 
       const boatImg = document.createElement("img");
-      boatImg.src = "https://raw.githubusercontent.com/velalabasso/carto-wix/main/voilier.jpg";
+      boatImg.src = "https://cdn.jsdelivr.net/gh/velalabasso/carto-wix@main/voilier.jpg";
       boatImg.alt = "voilier";
       Object.assign(boatImg.style, {
         width: "100%",
